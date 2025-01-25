662d39b0c1692525f03bdd55a732ca0ac7460703
LS7710 <luissoto7710@gmail.com>
2025-01-25 12:49:12 -0400

    fix for main file test

--- heap_insert/0-main.c
+++ heap_insert/0-main.c
@@ -1,26 +1,41 @@
-#include "binary_trees.h"
 #include <stdlib.h>
+#include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a new binary tree node
+ * _binary_tree_delete - Deallocate a binary tree
  *
- * @parent: Pointer to the parent node
- * @value: Value to put in the new node
- *
- * Return: Pointer to the new node, or NULL on failure
+ * @tree: Pointer to the root of the tree to delete
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+static void _binary_tree_delete(binary_tree_t *tree)
 {
-    binary_tree_t *new_node;
+    if (tree)
+    {
+        _binary_tree_delete(tree->left);
+        _binary_tree_delete(tree->right);
+        free(tree);
+    }
+}
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    binary_tree_t *root;
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+    root = binary_tree_node(NULL, 98);
 
-    return (new_node);
+    root->left = binary_tree_node(root, 12);
+    root->left->left = binary_tree_node(root->left, 6);
+    root->left->right = binary_tree_node(root->left, 16);
+
+    root->right = binary_tree_node(root, 402);
+    root->right->left = binary_tree_node(root->right, 256);
+    root->right->right = binary_tree_node(root->right, 512);
+
+    binary_tree_print(root);
+    _binary_tree_delete(root);
+    return (0);
 }
