367fb2a8a4df14f1aaff83e509fb652f6d6fe972
LS7710 <luissoto7710@gmail.com>
2025-01-25 13:09:57 -0400

    task 12 fix

--- heap_insert/binary_tree_print.c
+++ heap_insert/binary_tree_print.c
@@ -1,16 +1,11 @@
 #include "binary_trees.h"
 #include <stdio.h>
 
-/**
- * binary_tree_print - Prints a binary tree
- * @tree: Pointer to the root node of the tree
- */
 void binary_tree_print(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return;
 
-    /* Inorder traversal */
     binary_tree_print(tree->left);
     printf("%d\n", tree->n);
     binary_tree_print(tree->right);
