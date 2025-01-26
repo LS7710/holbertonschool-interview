8e43639f20cd67321661073abc2a9a252ab5b3d1
LS7710 <luissoto7710@gmail.com>
2025-01-26 12:06:49 -0400

    task 0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Calculate the minimum number of operations to achieve n H characters.
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
