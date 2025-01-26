3a7ff974fe404e49e4523cf01f6f1021781aabbc
LS7710 <luissoto7710@gmail.com>
2025-01-26 12:20:07 -0400

    pep fix

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,17 +3,18 @@
 Calculate the minimum number of operations to achieve n H characters.
 """
 
+
 def minOperations(n):
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     return operations
