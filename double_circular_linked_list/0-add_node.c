83fbd23e7bd271bdb5708ec8d43f61bf2103212f
LS7710 <luissoto7710@gmail.com>
2025-06-29 08:35:23 -0400

    first iterration of the task files code

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,73 @@
+#include "list.h"
+#include <string.h>
+
+/**
+ * create_node - Creates a new List node with given string
+ * @str: string to copy into node
+ *
+ * Return: pointer to new node or NULL on failure
+ */
+static List *create_node(char *str)
+{
+    List *node = malloc(sizeof(List));
+    if (!node)
+        return NULL;
+
+    node->str = strdup(str);
+    if (!node->str)
+    {
+        free(node);
+        return NULL;
+    }
+
+    node->next = node->prev = NULL;
+    return node;
+}
+
+/**
+ * add_node_end - Adds a node to the end of a circular doubly linked list
+ * @list: address of the pointer to the head
+ * @str: string to store in the new node
+ *
+ * Return: address of the new node or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+    List *new_node = create_node(str);
+    if (!new_node)
+        return NULL;
+
+    if (!*list)
+    {
+        new_node->next = new_node->prev = new_node;
+        *list = new_node;
+    }
+    else
+    {
+        List *tail = (*list)->prev;
+
+        new_node->next = *list;
+        new_node->prev = tail;
+        tail->next = new_node;
+        (*list)->prev = new_node;
+    }
+
+    return new_node;
+}
+
+/**
+ * add_node_begin - Adds a node to the beginning of a circular doubly linked list
+ * @list: address of the pointer to the head
+ * @str: string to store in the new node
+ *
+ * Return: address of the new node or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+    List *new_node = add_node_end(list, str);
+    if (!new_node)
+        return NULL;
+
+    *list = new_node;
+    return new_node;
+}
