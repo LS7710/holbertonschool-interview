b1b86180374b9df2e08c9610d01b0debf79663b7
LS7710 <luissoto7710@gmail.com>
2025-07-06 05:22:39 -0400

    betty

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -9,57 +9,86 @@
  */
 static int get_max(const int *array, size_t size)
 {
-    int max = array[0];
-    for (size_t i = 1; i < size; i++)
-        if (array[i] > max)
-            max = array[i];
-    return (max);
+	int max = array[0];
+	size_t i;
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+		{
+			max = array[i];
+		}
+	}
+
+	return (max);
 }
 
 /**
- * counting_sort - Performs a counting sort on array according to digit represented by exp
+ * counting_sort - Performs a counting sort on array according to digit
  * @array: Array to sort
  * @size: Number of elements in array
- * @exp: Exponent representing current digit place (1, 10, 100, etc)
+ * @exp: Exponent for current digit (1, 10, 100, etc)
  */
 static void counting_sort(int *array, size_t size, int exp)
 {
-    int *output = malloc(size * sizeof(int));
-    int count[10] = {0};
-    if (!output)
-        return;
+	int *output;
+	int count[10] = {0};
+	size_t i;
+	int digit;
 
-    /* Count occurrences */
-    for (size_t i = 0; i < size; i++)
-        count[(array[i] / exp) % 10]++;
+	output = malloc(size * sizeof(int));
+	if (!output)
+	{
+		return;
+	}
 
-    /* Accumulate counts */
-    for (int i = 1; i < 10; i++)
-        count[i] += count[i - 1];
+	for (i = 0; i < size; i++)
+	{
+		digit = (array[i] / exp) % 10;
+		count[digit]++;
+	}
 
-    /* Build output array */
-    for (size_t i = size; i > 0; i--)
-    {
-        int digit = (array[i - 1] / exp) % 10;
-        output[count[digit] - 1] = array[i - 1];
-        count[digit]--;
-    }
+	for (i = 1; i < 10; i++)
+	{
+		count[i] += count[i - 1];
+	}
 
-    /* Copy back and print */
-    for (size_t i = 0; i < size; i++)
-        array[i] = output[i];
-    print_array(array, size);
+	for (i = size; i > 0; i--)
+	{
+		digit = (array[i - 1] / exp) % 10;
+		output[count[digit] - 1] = array[i - 1];
+		count[digit]--;
+	}
 
-    free(output);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = output[i];
+	}
+
+	print_array(array, size);
+
+	free(output);
 }
 
+/**
+ * radix_sort - Sorts an array of integers in ascending order
+ * @array: Array to sort
+ * @size: Number of elements in array
+ */
 void radix_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	int max;
+	int exp;
 
-    int max = get_max(array, size);
-    for (int exp = 1; max / exp > 0; exp *= 10)
-        counting_sort(array, size, exp);
+	if (!array || size < 2)
+	{
+		return;
+	}
+
+	max = get_max(array, size);
+
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		counting_sort(array, size, exp);
+	}
 }
-
